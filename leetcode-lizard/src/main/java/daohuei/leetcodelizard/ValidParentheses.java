d3208f481733dd3b3393a6cad9260ea8db77ad3e
Ray Kao <raykao77@gmail.com>
2021-02-06 01:54:58 +0800

    feat(valid-parenthese): add description and basic structure

---
+++ leetcode-lizard/src/main/java/daohuei/leetcodelizard/ValidParentheses.java
@@ -0,0 +1,53 @@
+package daohuei.leetcodelizard.utils;
+
+import java.util.*
+
+/**
+ * Question: Valid Parentheses
+ * Description: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * Example 1: 
+ * Input: s = "()"
+ * Output: true
+ * 
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ * 
+ * Example 4:
+ * Input: s = "([)]"
+ * Output: false
+ * 
+ * Example 5:
+ * Input: s = "{[]}"
+ * Output: true
+ * 
+ * Constraints:
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'
+ */
+
+public class ValidParenthese{
+    /*
+     * Author: @RaymondKao
+     * 
+     * @Time_Complexity: O().
+     * 
+     * @Reason: .
+     * 
+     * @Space_Complexity: O().
+     * 
+     * @Reason: .
+     */
+    public static bool stack(String[] str){
+
+    }
+}
