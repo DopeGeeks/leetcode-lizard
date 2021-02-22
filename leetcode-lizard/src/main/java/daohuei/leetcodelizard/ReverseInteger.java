0714539ff8c4340ef4056231f9c15ae974e125dd
ballm06m06 <ballm06m06@yahoo.com.tw>
2021-02-22 18:10:26 +0800

    feat(Reverse Integer): Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1],  then return 0.

--- leetcode-lizard/src/main/java/daohuei/leetcodelizard/ReverseInteger.java
+++ leetcode-lizard/src/main/java/daohuei/leetcodelizard/ReverseInteger.java
@@ -35,7 +35,7 @@ public class ReverseInteger {
      * 
      * @Space: O(1), we only need to store result and prev
      */
-    public static int reverse(int x) {
+    public static int reverseByDaoHuei(int x) {
         // initialize result and previous value variable as 0
         int result = 0;
         int prev = 0;
@@ -57,6 +57,33 @@ public class ReverseInteger {
         }
         // afterward, we will get the reverse number!
         return result;
+    }
+
+    /*
+     * Author: @ballm06m06 Qusetion: Reverse Integer Description: Given a signed
+     * 32-bit integer x, return x with its digits reversed. If reversing x causes
+     * the value to go outside the signed 32-bit integer range [-231, 231 - 1], then
+     * return 0.
+     *
+     * Time Complexity: O(logn) ??? Space Complexity: O(1)
+     */
+    public int reverse(int x) {
+        // declare long cuz it's 64bit, while int is 32bits,
+        // so if we use int, it'll definitely overflow
+        long reverse = 0;
+
+        while (x != 0) {
+            int remainder = x % 10;
+            x = x / 10;
+            reverse = reverse * 10 + remainder;
+        }
+
+        // if it's overflow
+        if (reverse >= Integer.MAX_VALUE || reverse <= Integer.MIN_VALUE)
+            return 0;
+
+        return (int) reverse;
+
     }
 
 }
