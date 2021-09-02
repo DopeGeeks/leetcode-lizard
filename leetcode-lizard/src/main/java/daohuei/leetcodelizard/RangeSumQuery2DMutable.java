c5707ef5fb8951710d0d1e6b36c44710489d5335
Chih-Kai Ting <tingken0214@gmail.com>
2021-09-01 17:18:17 -0700

    feat(range-sum-query-mutable): add sol

---
+++ leetcode-lizard/src/main/java/daohuei/leetcodelizard/RangeSumQuery2DMutable.java
@@ -0,0 +1,172 @@
+package daohuei.leetcodelizard;
+
+/*
+ * 308. Range Sum Query 2D - Mutable
+ * Link: https://leetcode.com/problems/range-sum-query-2d-mutable/
+ */
+public class RangeSumQuery2DMutable {
+    class TreeNode {
+        int start;
+        int end;
+        int sum;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        // a node will have array's start index and end index and its accumulated sum of
+        // all its children (including itself)
+        public TreeNode(int left, int right, int sum) {
+            this.start = left;
+            this.end = right;
+            this.sum = sum;
+        }
+
+        public TreeNode(int left, int right) {
+            this.start = left;
+            this.end = right;
+            this.sum = 0;
+        }
+    }
+
+    /**
+     * @author: daohuei
+     * @description: accumulated difference with binary search tree
+     * @time: O(logn): update: log(n) , sumArray: log(n)
+     * @space: O(n): for binary search tree
+     */
+    public class BSTNumArray {
+        TreeNode root = null;
+
+        public BSTNumArray(int[] nums) {
+            if (nums == null || nums.length == 0)
+                return;
+            // build the tree
+            this.root = buildTree(nums, 0, nums.length - 1);
+        }
+
+        // for update value
+        void update(int i, int val) {
+            updateHelper(root, i, val);
+        }
+
+        void updateHelper(TreeNode root, int i, int val) {
+            if (root == null)
+                return;
+            // binary search to the target
+            int mid = root.start + (root.end - root.start) / 2;
+            if (i <= mid) {
+                updateHelper(root.leftChild, i, val);
+            } else {
+                updateHelper(root.rightChild, i, val);
+            }
+            // if there is only one node: the leaf is found
+            if (root.start == root.end && root.start == i) {
+                root.sum = val;
+                return;
+            }
+            // else update the root value
+            root.sum = root.leftChild.sum + root.rightChild.sum;
+        }
+
+        public int sumRange(int i, int j) {
+            return sumRangeHelper(root, i, j);
+        }
+
+        public int sumRangeHelper(TreeNode root, int i, int j) {
+            if (root == null || j < root.start || i > root.end || i > j)
+                return 0;
+            // if the range is exactly the current root's start and end
+            // then root's accumulated sum is the answer
+            if (i <= root.start && j >= root.end) {
+                return root.sum;
+            }
+            // binary search
+            int mid = root.start + (root.end - root.start) / 2;
+            int result = sumRangeHelper(root.leftChild, i, Math.min(mid, j))
+                    + sumRangeHelper(root.rightChild, Math.max(mid + 1, i), j);
+
+            return result;
+        }
+
+        public TreeNode buildTree(int[] nums, int i, int j) {
+            if (nums == null || nums.length == 0 || i > j)
+                return null;
+
+            // if only one node
+            if (i == j) {
+                return new TreeNode(i, j, nums[i]);
+            }
+
+            TreeNode current = new TreeNode(i, j);
+            // build up the tree with binary search
+            int mid = i + (j - i) / 2;
+
+            current.leftChild = buildTree(nums, i, mid);
+            current.rightChild = buildTree(nums, mid + 1, j);
+            // calculate the accumulated sum
+            current.sum = current.leftChild.sum + current.rightChild.sum;
+
+            return current;
+        }
+    }
+
+    /**
+     * @author: daohuei
+     * @description: using Fenwick tree/binary index tree for sum array
+     * @time: O(logn): update: log(n) , sumArray: log(n)
+     * @space: O(n): for binary index tree
+     */
+    public class FenwickTreeNumArray {
+
+        int[] btree;
+        int[] arr;
+
+        public FenwickTreeNumArray(int[] nums) {
+            // the binary index tree
+            btree = new int[nums.length + 1];
+            arr = nums;
+
+            for (int i = 0; i < nums.length; i++) {
+                // insert number into the binary index tree
+                add(i + 1, nums[i]);
+            }
+        }
+
+        void add(int i, int val) {
+            // j&-j, the right most 1
+            // 1 -> 1 1 -> 1
+            // 2 -> 2 10 -> 10
+            // 3 -> 1 11 -> 01
+            // 4 -> 4 100 -> 100
+            // 5 -> 1 101 -> 001
+            // 6 -> 2 110 -> 010
+            // 7 -> 1 111 -> 001
+            // 8 -> 8 1000 -> 1000
+            // 9 -> 1 1001 -> 0001
+            // 10 -> 2 1010 -> 0010
+            // 11 -> 1 1011 -> 0001
+            // 12 -> 4 1100 -> 0100
+            // this will go through every index that has accumulated sum need this number
+            for (int j = i; j < btree.length; j = j + (j & (-j))) {
+                btree[j] += val;
+            }
+        }
+
+        void update(int i, int val) {
+            add(i + 1, val - arr[i]);
+            arr[i] = val;
+        }
+
+        public int sumRange(int i, int j) {
+            return sumHelper(j + 1) - sumHelper(i);
+        }
+
+        public int sumHelper(int i) {
+            int sum = 0;
+            // if we sum up, we just go through the binary accumulated sum
+            for (int j = i; j >= 1; j = j - (j & (-j))) {
+                sum += btree[j];
+            }
+            return sum;
+        }
+    }
+}
--- leetcode-lizard/src/main/java/daohuei/leetcodelizard/RangeSumQueryMutable.java
+++ leetcode-lizard/src/main/java/daohuei/leetcodelizard/RangeSumQueryMutable.java
@@ -1,172 +1,96 @@
 package daohuei.leetcodelizard;
 
 /*
- * 307. Range Sum Query – Mutable
- * Link: https://leetcode.com/problems/range-sum-query-2d-mutable/
+ * 307. Range Sum Query - Mutable
+ * Link: https://leetcode.com/problems/range-sum-query-mutable/description/
  */
+
 public class RangeSumQueryMutable {
-    class TreeNode {
-        int start;
-        int end;
-        int sum;
-        TreeNode leftChild;
-        TreeNode rightChild;
+    class NumArray {
+        /**
+         * @author: daohuei
+         * @description: segment tree
+         * @time: O(n/logn): n for build the segment tree by DFS, logn for each query
+         *        with binary search
+         * @space: O(n): for segment tree
+         */
+        SegTreeNode segTree;
 
-        // a node will have array's start index and end index and its accumulated sum of
-        // all its children (including itself)
-        public TreeNode(int left, int right, int sum) {
-            this.start = left;
-            this.end = right;
-            this.sum = sum;
-        }
-
-        public TreeNode(int left, int right) {
-            this.start = left;
-            this.end = right;
-            this.sum = 0;
-        }
-    }
-
-    /**
-     * @author: daohuei
-     * @description: accumulated difference with binary search tree
-     * @time: O(logn): update: log(n) , sumArray: log(n)
-     * @space: O(n): for binary search tree
-     */
-    public class BSTNumArray {
-        TreeNode root = null;
-
-        public BSTNumArray(int[] nums) {
+        public NumArray(int[] nums) {
+            // empty case
             if (nums == null || nums.length == 0)
                 return;
-            // build the tree
-            this.root = buildTree(nums, 0, nums.length - 1);
+            segTree = build(nums, 0, nums.length - 1);
         }
 
-        // for update value
-        void update(int i, int val) {
-            updateHelper(root, i, val);
+        public void update(int index, int val) {
+            updateNode(segTree, index, val);
         }
 
-        void updateHelper(TreeNode root, int i, int val) {
-            if (root == null)
+        public int sumRange(int left, int right) {
+            return rangeQuery(segTree, left, right);
+        }
+
+        class SegTreeNode {
+            int start, end, sum;
+            SegTreeNode left, right;
+
+            public SegTreeNode(int start, int end, int sum, SegTreeNode left, SegTreeNode right) {
+                // start and end for the segment
+                this.start = start;
+                this.end = end;
+                // the sum of the segment
+                this.sum = sum;
+                // left node and right node
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        // build the seg tree
+        private SegTreeNode build(int[] nums, int start, int end) {
+            // if only 1 number, no left and right nodes
+            if (start == end)
+                return new SegTreeNode(start, end, nums[start], null, null);
+            // get the mid index
+            int mid = start + (end - start) / 2;
+            // build with dfs
+            SegTreeNode left = build(nums, start, mid);
+            SegTreeNode right = build(nums, mid + 1, end);
+            // sum the left node and the right node
+            return new SegTreeNode(start, end, left.sum + right.sum, left, right);
+        }
+
+        //
+        private void updateNode(SegTreeNode node, int i, int val) {
+            // if the node reach the leaf, update the sum with new value
+            if (node.start == i && node.end == i) {
+                node.sum = val;
                 return;
-            // binary search to the target
-            int mid = root.start + (root.end - root.start) / 2;
-            if (i <= mid) {
-                updateHelper(root.leftChild, i, val);
-            } else {
-                updateHelper(root.rightChild, i, val);
             }
-            // if there is only one node: the leaf is found
-            if (root.start == root.end && root.start == i) {
-                root.sum = val;
-                return;
-            }
-            // else update the root value
-            root.sum = root.leftChild.sum + root.rightChild.sum;
+            // if less or equal than left's end, try left
+            if (i <= node.left.end)
+                updateNode(node.left, i, val);
+            // else just try right
+            else
+                updateNode(node.right, i, val);
+            // update the sum
+            node.sum = node.left.sum + node.right.sum;
         }
 
-        public int sumRange(int i, int j) {
-            return sumRangeHelper(root, i, j);
-        }
-
-        public int sumRangeHelper(TreeNode root, int i, int j) {
-            if (root == null || j < root.start || i > root.end || i > j)
-                return 0;
-            // if the range is exactly the current root's start and end
-            // then root's accumulated sum is the answer
-            if (i <= root.start && j >= root.end) {
-                return root.sum;
-            }
-            // binary search
-            int mid = root.start + (root.end - root.start) / 2;
-            int result = sumRangeHelper(root.leftChild, i, Math.min(mid, j))
-                    + sumRangeHelper(root.rightChild, Math.max(mid + 1, i), j);
-
-            return result;
-        }
-
-        public TreeNode buildTree(int[] nums, int i, int j) {
-            if (nums == null || nums.length == 0 || i > j)
-                return null;
-
-            // if only one node
-            if (i == j) {
-                return new TreeNode(i, j, nums[i]);
-            }
-
-            TreeNode current = new TreeNode(i, j);
-            // build up the tree with binary search
-            int mid = i + (j - i) / 2;
-
-            current.leftChild = buildTree(nums, i, mid);
-            current.rightChild = buildTree(nums, mid + 1, j);
-            // calculate the accumulated sum
-            current.sum = current.leftChild.sum + current.rightChild.sum;
-
-            return current;
-        }
-    }
-
-    /**
-     * @author: daohuei
-     * @description: using Fenwick tree/binary index tree for sum array
-     * @time: O(logn): update: log(n) , sumArray: log(n)
-     * @space: O(n): for binary index tree
-     */
-    public class FenwickTreeNumArray {
-
-        int[] btree;
-        int[] arr;
-
-        public FenwickTreeNumArray(int[] nums) {
-            // the binary index tree
-            btree = new int[nums.length + 1];
-            arr = nums;
-
-            for (int i = 0; i < nums.length; i++) {
-                // insert number into the binary index tree
-                add(i + 1, nums[i]);
-            }
-        }
-
-        void add(int i, int val) {
-            // j&-j, the right most 1
-            // 1 -> 1 1 -> 1
-            // 2 -> 2 10 -> 10
-            // 3 -> 1 11 -> 01
-            // 4 -> 4 100 -> 100
-            // 5 -> 1 101 -> 001
-            // 6 -> 2 110 -> 010
-            // 7 -> 1 111 -> 001
-            // 8 -> 8 1000 -> 1000
-            // 9 -> 1 1001 -> 0001
-            // 10 -> 2 1010 -> 0010
-            // 11 -> 1 1011 -> 0001
-            // 12 -> 4 1100 -> 0100
-            // this will go through every index that has accumulated sum need this number
-            for (int j = i; j < btree.length; j = j + (j & (-j))) {
-                btree[j] += val;
-            }
-        }
-
-        void update(int i, int val) {
-            add(i + 1, val - arr[i]);
-            arr[i] = val;
-        }
-
-        public int sumRange(int i, int j) {
-            return sumHelper(j + 1) - sumHelper(i);
-        }
-
-        public int sumHelper(int i) {
-            int sum = 0;
-            // if we sum up, we just go through the binary accumulated sum
-            for (int j = i; j >= 1; j = j - (j & (-j))) {
-                sum += btree[j];
-            }
-            return sum;
+        private int rangeQuery(SegTreeNode node, int i, int j) {
+            // if the node is exactly at that index, the sum is the value
+            if (node.start == i && node.end == j)
+                return node.sum;
+            // try left or right based on conditions
+            if (j <= node.left.end)
+                return rangeQuery(node.left, i, j);
+            else if (i >= node.right.start)
+                return rangeQuery(node.right, i, j);
+            // or else get the mid point
+            int mid = node.start + (node.end - node.start) / 2;
+            // query left and right with the slpit mid point
+            return rangeQuery(node.left, i, mid) + rangeQuery(node.right, mid + 1, j);
         }
     }
 
